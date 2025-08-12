cb50497ecc700891a24f9f8e277700912f046a17
Abel Maquera Sacaca <36040950+AbelRM@users.noreply.github.com>
2025-08-11 21:32:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://github.com/AbelRM/app-vue-deploy'
+  base: 'https://AbelRM.github.io/app-vue-deploy'
 })
